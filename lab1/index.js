325a4e6ee48a9c5c3953c84732086cb3918fb2d7
sobolink-spec <sobolin_k@iuca.kg>
2025-09-15 13:55:14 +0600

    first commit

---
+++ lab1/index.js
@@ -0,0 +1,9 @@
+var enterUserName = prompt('Введите свое имя:')
+var enterUserSurname = prompt('Введите свою фамилию:')
+var enterUserAge = prompt('Введите свой возраст:')
+
+//console.log("Я ", enterUserName, " ", enterUserSurname,", мне ", enterUserAge," лет.")
+alert(enterUserName)
+alert(enterUserSurname)
+alert(enterUserAge)
+//document.writeln("Я ", enterUserName, " ", enterUserSurname,", мне ", enterUserAge," лет.")
